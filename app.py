9f1a0fdfd97e70b5c6df5e9f712fdd1e0e30a01d
Sudy37z <sudhanshuthakur8137@gmail.com>
2023-06-12 21:43:24 +0530

    JSON data updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for,render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pandas as pd
 
@@ -9,22 +9,28 @@ regmodel = pickle.load(open('regmodel.pkl', 'rb'))
 encoder = pickle.load(open('encoder.pkl', 'rb'))
 
 @app.route('/')
-           
 def home():
-        return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
-
-
 def predict_api():
-        data = request.json['data']
-        print(data)
-        print(np.array(list(data.value())))
-        cols = ['experience_level', 'employment_type', 'job_title','employee_residence','company_location','company_size']
-        data[cols]=data[cols].apply(encoder().fit_transform)
-        output = regmodel.predict(data)
-        print(output)
-        return jsonify(output)
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())))
+    cols = ['experience_level', 'employment_type', 'remote_ratio', 'company_size']
+    for col in cols:
+        data[f'EN_{col}'] = encoder.fit_transform(data[col])
+    
+    def EN_mean_salary(x):
+        cols = ['job_title', 'employee_residence', 'company_location']
+        base = 'salary_in_usd'
+        for col in cols:
+            x[f'EN_mean_{col}'] = data.groupby(col)[base].transform('mean')
+        return x
 
-if __name__ =='__main__':
-        app.run(debug=True)
+    output = regmodel.predict(data)
+    print(output)
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
