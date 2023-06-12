3a8a4632ae2e68429f3a255b702a89299eac1919
Sudy37z <sudhanshuthakur8137@gmail.com>
2023-06-12 10:20:53 +0530

    Post method done

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ encoder = pickle.load(open('encoder.pkl', 'rb'))
 def home():
         return render_template('home.html')
 
-@app.route('/predict_api',method=['POST'])
+@app.route('/predict_api', methods=['POST'])
+
 
 def predict_api():
         data = request.json['data']
         print(data)
         print(np.array(list(data.value())))
-        cols = ['experience_level', 'employment_type', 'job_title','salary_currency','employee_residence','company_location','company_size']
+        cols = ['experience_level', 'employment_type', 'job_title','employee_residence','company_location','company_size']
         data[cols]=data[cols].apply(encoder().fit_transform)
         output = regmodel.predict(data)
         print(output)
