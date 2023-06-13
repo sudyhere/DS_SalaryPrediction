4567c48289d6aeade37bcbba53ef328c55304e7a
Sudy37z <sudhanshuthakur8137@gmail.com>
2023-06-13 22:13:09 +0530

    JSON Input changed

--- app.py
+++ app.py
@@ -17,17 +17,6 @@ def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())))
-    cols = ['experience_level', 'employment_type', 'remote_ratio', 'company_size']
-    for col in cols:
-        data[f'EN_{col}'] = encoder.fit_transform(data[col])
-    
-    def EN_mean_salary(x):
-        cols = ['job_title', 'employee_residence', 'company_location']
-        base = 'salary_in_usd'
-        for col in cols:
-            x[f'EN_mean_{col}'] = data.groupby(col)[base].transform('mean')
-        return x
-
     output = regmodel.predict(data)
     print(output)
     return jsonify(output)
